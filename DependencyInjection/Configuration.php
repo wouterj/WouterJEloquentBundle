9bd099bff897c1030e97c4110307022668508a1f
WouterJ <waldio.webdesign@gmail.com>
2014-02-27 11:42:41 +0100

    Another refactor fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $root = $treeBuilder->root('wj_eloquent');
+        $root = $treeBuilder->root('wouterj_eloquent');
 
         $this->addAliasesSection($root);
         $this->addCapsuleSection($root);
