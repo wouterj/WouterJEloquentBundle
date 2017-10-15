f09e1f523714a961a925f8e896907417369578b8
Wouter J <wouter@wouterj.nl>
2017-10-15 14:27:11 +0200

    Added make:seeder command

---
+++ tests/Fixtures/seeds/PostSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Seed;
+
+use WouterJ\EloquentBundle\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
