9e6cb2afe5f097cfa4e67e9edac3a51d0d63734e
Wouter de Jong <wouter@wouterj.nl>
2022-11-05 12:06:39 +0100

    Fixed tests for SymfonyMakerBundle 1.44+

--- tests/Maker/SeederMakeCommandTest.php
+++ tests/Maker/SeederMakeCommandTest.php
@@ -4,6 +4,7 @@ namespace WouterJ\EloquentBundle\Maker;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\MakerBundle\DependencyBuilder;
 use Symfony\Bundle\MakerBundle\FileManager;
 use Symfony\Bundle\MakerBundle\Generator;
 use Symfony\Bundle\MakerBundle\Util\PhpCompatUtil;
@@ -22,7 +23,13 @@ class SeederMakeCommandTest extends TestCase
     {
         $this->fileManager = \Mockery::spy(FileManager::class);
         $this->maker = new MakeSeeder($this->fileManager);
-        $this->generator = new Generator($this->fileManager, 'App', class_exists(PhpCompatUtil::class) ? new PhpCompatUtil($this->fileManager) : null);
+
+        $phpCompatUtil = null;
+        if (class_exists(PhpCompatUtil::class) && method_exists(DependencyBuilder::class, 'isPhpVersionSatisfied')) {
+            // PHP compat util must be used in SymfonyMakerBundle >=1.22,<1.44 and we need at most 1.43 for PHP <8.0 support testing
+            $phpCompatUtil = new PhpCompatUtil($this->fileManager);
+        }
+        $this->generator = new Generator($this->fileManager, 'App', $phpCompatUtil);
     }
 
     /**
