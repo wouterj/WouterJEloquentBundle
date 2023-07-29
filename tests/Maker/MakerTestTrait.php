00a6308945d9c856a5d384e9c676909410d0e8b7
Wouter de Jong <wouter@wouterj.nl>
2023-07-29 18:26:06 +0200

    Fix tests after new MakerBundle release

--- tests/Maker/MakerTestTrait.php
+++ tests/Maker/MakerTestTrait.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\MakerBundle\Command\MakerCommand;
 use Symfony\Bundle\MakerBundle\FileManager;
 use Symfony\Bundle\MakerBundle\Generator;
 use Symfony\Bundle\MakerBundle\MakerInterface;
+use Symfony\Bundle\MakerBundle\Util\TemplateLinter;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\ApplicationTester;
 
@@ -19,7 +20,7 @@ trait MakerTestTrait
         $app = new Application();
         $app->setAutoExit(false);
         $app->add(
-            (new MakerCommand($this->maker, \Mockery::spy(FileManager::class), $this->generator ?? \Mockery::spy(Generator::class)))
+            (new MakerCommand($this->maker, \Mockery::spy(FileManager::class), $this->generator ?? \Mockery::spy(Generator::class), class_exists(TemplateLinter::class) ? new TemplateLinter() : null))
                 ->setName($this->maker->getCommandName())
         );
 
